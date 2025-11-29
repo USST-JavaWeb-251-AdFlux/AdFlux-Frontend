0a5cc8e36b9bfb0cc0d20d76175e830c116dcd75
BobLiu <bobliu@bobliu.tech>
2025-11-29 23:54:11 +0800

    feat: use dynamic page title (#2)

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
+import { useRoute } from 'vue-router';
+import { useTitle } from '@vueuse/core';
 import zhCn from 'element-plus/es/locale/lang/zh-cn';
+
+const route = useRoute();
+const title = computed(() => {
+    const baseTitle = 'AdFlux';
+    const routeTitle = route.meta.title;
+    return routeTitle ? `${routeTitle} | ${baseTitle}` : baseTitle;
+});
+useTitle(title);
 </script>
 
 <template>

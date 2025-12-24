93db68dc2d95d1c7aa877fef15bd129e6a846cfb
BobLiu <bobliu@bobliu.tech>
2025-12-24 11:09:17 +0800

    fix: import notify elements globally to avoid style missing

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+import 'element-plus/theme-chalk/src/message-box.scss';
+import 'element-plus/theme-chalk/src/message.scss';
+import 'element-plus/theme-chalk/src/notification.scss';
 import App from '@/App.vue';
 import router from '@/router';
 import '@/styles/element.scss';

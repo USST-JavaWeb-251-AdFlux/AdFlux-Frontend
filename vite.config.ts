0cfef284ff47911bf306e460e44dd4d9a6aabcbc
BobLiu <bobliu@bobliu.tech>
2025-12-24 13:20:09 +0800

    feat: update vite config for better build process (#34)
    * chore: fix vite failed when host env setting to relative path
    
    * chore: set echart as manual chunk when building dist to reduce package size

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,11 @@ import AutoImport from 'unplugin-auto-import/vite';
 import ElementPlus from 'unplugin-element-plus/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 import Components from 'unplugin-vue-components/vite';
-import { defineConfig, loadEnv } from 'vite';
+import { type UserConfig, defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd());
-    const apiHost = new URL(env.VITE_API_HOST);
-
-    return {
+    const config: UserConfig = {
         plugins: [
             vue(),
             AutoImport({
@@ -44,16 +41,39 @@ export default defineConfig(({ mode }) => {
                 '@': fileURLToPath(new URL('./src', import.meta.url)),
             },
         },
+        build: {
+            chunkSizeWarningLimit: 512,
+            rollupOptions: {
+                output: {
+                    manualChunks(id) {
+                        if (id.includes('node_modules')) {
+                            if (id.includes('echarts')) {
+                                return 'echarts';
+                            }
+                        }
+                    },
+                },
+            },
+        },
         server: {
             host: '127.0.0.1',
             port: 5173,
             strictPort: true,
-            proxy: {
+        },
+    };
+
+    try {
+        const env = loadEnv(mode, process.cwd());
+        const apiHost = new URL(env.VITE_API_HOST);
+        if (config.server) {
+            config.server.proxy = {
                 [apiHost.pathname]: {
                     target: apiHost.origin,
                     changeOrigin: true,
                 },
-            },
-        },
-    };
+            };
+        }
+    } catch {}
+
+    return config;
 });

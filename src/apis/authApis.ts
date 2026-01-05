172040743684018ad9ba5bc9ef9637211c22768d
BobLiu <bobliu@bobliu.tech>
2026-01-05 22:56:58 +0800

    feat: rename publisher (#66)

--- src/apis/authApis.ts
+++ src/apis/authApis.ts
@@ -5,7 +5,7 @@ import type { ApiResponse } from './types';
 export const UserRole = createEnum({
     admin: { value: 'admin', label: '管理员', color: '#E91E63' },
     advertiser: { value: 'advertiser', label: '广告主', color: '#3F51B5' },
-    publisher: { value: 'publisher', label: '发布主', color: '#009688' },
+    publisher: { value: 'publisher', label: '站长', color: '#009688' },
 } as const);
 
 export type UserInfo = {
